93d93079f2ed9fb9c325f287d291bafc331775a9
edrian <edrianssyahputra@gmail.com>
2025-12-19 02:38:13 +0700

    set file and regist driver, not yet connect mysql

---
+++ src/main/java/Drian/Ticket/App/App.java
@@ -0,0 +1,11 @@
+package Drian.Ticket.App;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Ini perubahan kedua");
+    }
+}
---
+++ src/test/java/Drian/Ticket/jdbcTest.java
@@ -0,0 +1,31 @@
+package Drian.Ticket;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import java.sql.Connection;
+
+
+public class jdbcTest {
+    @Test
+    public void jdbcConnect() {
+        try {
+            Driver mysqlDriver = new com.mysql.cj.jdbc.Driver();
+            DriverManager.registerDriver(mysqlDriver);
+
+        } catch (SQLException Exception) {
+            Exception.printStackTrace();
+    }
+
+    String jdbcUrl = "jdbc:mysql://localhost:3306/tabel_barang";
+    String username = "root";
+    String password = "";
+    try (Connection conn = DriverManager.getConnection(jdbcUrl, username, password)) {
+        System.out.println("Database successfully connected!");
+    } catch (SQLException Exception) {
+        Assertions.fail(Exception);
+    }
+}
+}
